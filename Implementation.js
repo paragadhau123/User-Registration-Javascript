2849564a1727414d933694cf553d01a374fb5de4
Parag Adhau <adhauparag64@gmail.com>
2020-09-27 12:48:28 +0530

    Added :The welcome message

---
+++ Implementation.js
@@ -0,0 +1,4 @@
+class Implementation{
+
+}
+module.exports = new Implementation();
---
+++ Main.js
@@ -0,0 +1,8 @@
+const print = require('./Implementation');
+class main {
+    main() {
+        console.log("Welcome to User Validation");
+    }
+}
+const object = new main();
+object.main();
