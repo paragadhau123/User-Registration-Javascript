7bdfcbb70b68dfb4cc272c4f95d5d68b465281e7
Parag Adhau <adhauparag64@gmail.com>
2020-09-27 18:29:44 +0530

    Added :Code for validating users first name

--- Implementation.js
+++ Implementation.js
@@ -1,4 +1,15 @@
-class Implementation{
 
+class Implementation {
+
+    firstNameValidator(name) {
+        let FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+
+        if (FIRST_NAME_PATTERN.test(name)) {
+            console.log("valid First Name");
+        }
+        else {
+            console.log("Invalid First Name");
+        }
+    }
 }
 module.exports = new Implementation();
--- Main.js
+++ Main.js
@@ -1,7 +1,29 @@
 const print = require('./Implementation');
 class main {
     main() {
-        console.log("Welcome to User Validation");
+        console.log("---Welcome to User Validation---\n");
+        console.log("\t--MENU--");
+        console.log("1: Validate First Name          ");
+        console.log("2: Validate Second Name         ");
+        console.log("3: Validate Email Address       ");
+        console.log("4: Validate Phone Number        ");
+        console.log("5: Validate Password            ");
+        console.log("6: Exit                       \n");
+        console.log("---Enter Your Choice---         ");
+        var readline = require('readline');
+        var input = readline.createInterface(process.stdin, process.stdout);
+        input.question("Enter Choice :", function (option) {
+            switch (option) {
+                case "1":
+                    input.question("Enter First Name: \n", function (firstName) {
+                        print.firstNameValidator(firstName);
+                    })
+                    break;
+                default:
+                    console.log("Enter Valid option");
+                    break;
+            }
+        })
     }
 }
 const object = new main();
