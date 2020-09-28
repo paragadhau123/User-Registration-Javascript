e78ed59456639f5febb72b86c5cb749444793997
Parag Adhau <adhauparag64@gmail.com>
2020-09-28 21:53:18 +0530

    Refactored :Code for using ES6 features

--- Implementation.js
+++ Implementation.js
@@ -1,57 +1,68 @@
+const FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+const LAST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+const EMAIL_PATTERN = new RegExp("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$");
+const PHONE_NUMBER_PATTERN = new RegExp("^([0-9]{2}[ ]+)?[0-9]{10}$");
+const PASSWORD_PATTERN = new RegExp("^([A-Z]{1,})*([(@#$*)-])*[a-zA-Z0-9]{6,}$");
+
 class Implementation {
 
-    firstNameValidator(name) {
-        let FIRST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+    firstNameValidator = (firstname) => {
 
-        if (FIRST_NAME_PATTERN.test(name)) {
+        if (FIRST_NAME_PATTERN.test(firstname)) {
             console.log("valid First Name");
+            return true;
         }
         else {
             console.log("Invalid First Name");
+            return false;
         }
     }
 
-    lastNameValidator(name) {
-        let LAST_NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
+    lastNameValidator = (lastname) => {
 
-        if (LAST_NAME_PATTERN.test(name)) {
+        if (LAST_NAME_PATTERN.test(lastname)) {
             console.log("valid Last Name");
+            return true;
         }
         else {
             console.log("Invalid Last Name");
+            return false;
         }
     }
 
-    emailValidator(email) {
-        let EMAIL_PATTERN = new RegExp("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$");
+    emailValidator = (email) => {
 
         if (EMAIL_PATTERN.test(email)) {
             console.log("valid Email");
+            return true;
         }
         else {
             console.log("Invalid Email");
+            return false;
         }
     }
 
-    phoneNumberValidator(phoneNumber) {
-        let PHONE_NUMBER_PATTERN = new RegExp("^([0-9]{2}[ ]+)?[0-9]{10}$");
+    phoneNumberValidator = (phoneNumber) => {
 
         if (PHONE_NUMBER_PATTERN.test(phoneNumber)) {
             console.log("valid Phone Number");
+            return true;
         }
         else {
             console.log("Invalid Phone Number");
+            return false;
         }
     }
 
-    passwordValidator(password) {
-        let PASSWORD_PATTERN = new RegExp("^([A-Z]{1,})*([(@#$*)-])*[a-zA-Z0-9]{6,}$");
+    passwordValidator = (password) => {
 
         if (PASSWORD_PATTERN.test(password)) {
             console.log("valid Password");
+            return true;
         }
         else {
             console.log("Invalid Password");
+            return false;
         }
     }
 }
--- Main.js
+++ Main.js
@@ -1,6 +1,10 @@
 const print = require('./Implementation');
+var readline = require('readline');
+var input = readline.createInterface(process.stdin, process.stdout);
+
 class Main {
-    main() {
+
+    main = () => {
         console.log("---Welcome to User Validation---\n");
         console.log("\t--MENU--");
         console.log("1: Validate First Name          ");
@@ -8,42 +12,51 @@ class Main {
         console.log("3: Validate Email Address       ");
         console.log("4: Validate Phone Number        ");
         console.log("5: Validate Password            ");
-        console.log("6: Exit                       \n");
         console.log("---Enter Your Choice---         ");
-        var readline = require('readline');
-        var input = readline.createInterface(process.stdin, process.stdout);
         input.question("Enter Choice :", function (option) {
             switch (option) {
+
                 case "1":
                     input.question("Enter First Name: \n", function (firstName) {
-                        print.firstNameValidator(firstName);
+                        Boolean = print.firstNameValidator(firstName);
+                        process.exit();
                     })
                     break;
+
                 case "2":
                     input.question("Enter Last Name: \n", function (lastName) {
                         print.lastNameValidator(lastName);
+                        process.exit();
                     })
                     break;
+
                 case "3":
                     input.question("Enter Email Address: \n", function (email) {
                         print.emailValidator(email);
+                        process.exit();
                     })
                     break;
+
                 case "4":
                     input.question("Enter Phone Number: \n", function (phoneNumber) {
                         print.phoneNumberValidator(phoneNumber);
+                        process.exit();
                     })
                     break;
+
                 case "5":
                     input.question("Enter Password: \n", function (password) {
                         print.passwordValidator(password);
+                        process.exit();
                     })
                     break;
+
                 default:
                     console.log("Enter Valid option");
                     break;
             }
         })
+
     }
 }
 const object = new Main();
